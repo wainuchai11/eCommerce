efbc60e7c12ad3c5aa41b374341224402c9bd46e
wainuchai <jack.winuchai@gmail.com>
2022-11-05 04:06:24 +0700

    init

---
+++ layouts/default.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app>
+    <v-main
+      dark
+      :style="{ backgroundColor: $vuetify.theme.dark ? '#0a0514' : 'white' }"
+    >
+      <Nuxt />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "DefaultLayout",
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: "mdi-apps",
+          title: "Welcome",
+          to: "/",
+        },
+        {
+          icon: "mdi-chart-bubble",
+          title: "Inspire",
+          to: "/inspire",
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: "Vuetify.js",
+    };
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <Nav />
+    <home-carousel :sale_items="sale_item" />
+    <br />
+    <v-container>
+      <h1 class="text-md-h4 text-h6">Check these out</h1>
+      <br />
+      <ProductSlider :products="products" />
+    </v-container>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from "../components/Footer.vue";
+import HomeCarousel from "../components/HomeCarousel.vue";
+import Nav from "../components/Nav.vue";
+import ProductSlider from "../components/ProductSlider.vue";
+
+export default {
+  async created() {
+    this.sale_item = await this.$content("products")
+      .where({ onSale: true })
+      .fetch();
+    this.products = await this.$content("products").fetch();
+  },
+  data() {
+    return {
+      products: null,
+      sale_item: null,
+    };
+  },
+  components: { Nav, HomeCarousel, Footer, Nav, Footer, ProductSlider },
+};
+</script>
+
+<style>
+</style>
